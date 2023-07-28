04f1359e3d4188401f0a83a571d29fb96d916f7a
CherylM <cmuschallik@gmail.com>
2023-07-28 15:57:09 -0400

    edits to about page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import coverImage from '../../assets/cover/pexels-hoang-le-978484.jpg';
+import coverImage from '../../assets/cover/pexels-free-nature-stock-567584.jpg';
 
 function Header(props) {
 
   return (
     <header className="flex-row space-between px-1">
-      <h1>Cheryl</h1>
+      <h1 class="px-1">Cheryl</h1>
       <img src={coverImage} alt="wooden background"></img>
       {props.children}
     </header>
